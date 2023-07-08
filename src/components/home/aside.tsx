121bec15f2a119506e025e33778df7f8f28bfd4f
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-08 23:21:36 +0530

    Aside

---
+++ src/components/home/aside.tsx
@@ -0,0 +1,11 @@
+export default function aside() {
+  return (
+    <>
+
+       <div class="bg-red-500  h-32 w-full"></div>
+      <div class="bg-purple-500  h-72 w-full"></div>
+      <div class="bg-black grow"></div>
+      
+    </>
+  )
+}
