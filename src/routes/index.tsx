1e4dff2ffb993481fb3c47284f70af85a111b217
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-08 23:22:05 +0530

    index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,86 +1,24 @@
+import Sidebar from "~/components/home/sidebar";
+import Content from "~/components/home/content";
+import Aside from "~/components/home/aside";
+
+
+
 export default() => {
   return (
     <>
-      <div class="flex bg-green-500 min-h-screen w-full">
-    <div class="flex flex-col bg-purple-500 w-56 gap-3 pt-3">
-        <h1 class=" pt-10 pl-6 text-white font-bold">E@Crouse</h1>
-        <div class=" bg-purple-500 flex  flex-col grow gap-3 items-center pt-28">
-        <div class="bg-white h-12 w-52"></div>
-        <div class="bg-white h-12 w-52"></div>
-        <div class="bg-white h-12 w-52"></div>
-        <div class="bg-white h-12 w-52"></div>
-        <div class="bg-white h-12 w-52"></div>
-
-     </div>
-     <div class="  bg-purple-500 w-full flex gap-4 flex-col items-center pb-3">
-        <div class="bg-white h-12 w-52"></div>
-        <div class="bg-white h-12 w-52"></div>
-     </div>
-
-</div>
-
-
-
-
-<div class="bg-white grow flex flex-col px-10 gap-7">
-    
-    <div class="h-14 bg-white flex gap-4 mt-7  ">
-
-        <div class="bg-red-400  grow "></div>
-        <div class="bg-red-400  h-full w-24"></div>
-        <div class="bg-red-400  h-full w-24"></div>
-     
-      </div>
-      <div class="h-14 bg-white flex   justify-between ">
-
-        <div class="bg-red-400  h-full w-32 "></div>
-        <div class="bg-red-400  h-full w-32"></div>
-        <div class="bg-red-400  h-full w-32"></div>
-        <div class="bg-red-400  h-full w-32 "></div>
-        <div class="bg-red-400  h-full w-32 "></div>
-     
-      </div>
-
-    <div class="bg-gray-500 h-40 flex  1 rounded-3xl ">
-        <div class="bg-red-400 h-40 rounded-3xl w-72"></div>
-        <div class=" bg-purple-600 rounded-3xl flex grow flex-col">
-           <div class="h-14 w-full bg-red-500"></div>
-           <div class=" bg-purple-600 grow"></div>
-           <div class="h-10 w-full bg-pink-500"></div>
+      <div class="flex bg-green-500 min-h-screen w-full ">
+           <Sidebar></Sidebar>
+         <div class="bg-white flex flex-col px-10 gap-7  grow">
+             <Content></Content>
         </div>
+         <div class="bg-gray-700 w-72 lg:flex flex-col hidden">
+               <Aside></Aside>
+            </div>
+
+
+
       </div>
-      <div class="bg-gray-500 h-40 flex  1 rounded-3xl ">
-        <div class="bg-red-400 h-40 rounded-3xl w-72"></div>
-        <div class=" bg-purple-600 rounded-3xl flex grow flex-col">
-           <div class="h-14 w-full bg-red-500"></div>
-           <div class=" bg-purple-600 grow"></div>
-           <div class="h-10 w-full bg-pink-500"></div>
-        </div>
-      </div>
-      <div class="bg-gray-500 h-40 flex  1 rounded-3xl ">
-        <div class="bg-red-400 h-40 rounded-3xl w-72"></div>
-        <div class=" bg-purple-600 rounded-3xl flex grow flex-col">
-           <div class="h-14 w-full bg-red-500"></div>
-           <div class=" bg-purple-600 grow"></div>
-           <div class="h-10 w-full bg-pink-500"></div>
-        </div>
-      </div>
- </div>
-
-
-
-    <div class="bg-gray-700 w-72 flex flex-col">
-      
-      <div class="bg-red-500  h-32 w-full"></div>
-      <div class="bg-purple-500  h-72 w-full"></div>
-      <div class="bg-black grow"></div>
-
-        
-     </div>
-
-
-
-  </div>
 
     </>
   );
