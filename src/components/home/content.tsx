378687f5b19ff685a79030d0a04154e7b96c565d
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-08 23:21:51 +0530

    content

---
+++ src/components/home/content.tsx
@@ -0,0 +1,61 @@
+export default function content() {
+  return (
+    <>
+    <div class="h-14 bg-white flex gap-4 mt-7  ">
+
+      <div class="bg-red-400  grow "></div>
+      <div class="flex  gap-3 ">
+      <div class="bg-red-400  h-full lg:w-24 w-10 "></div>
+      <div class="bg-red-400  h-full lg:w-24  w-10"></div>
+      </div>
+    </div>
+    <div class="h-14 bg-white flex   justify-between lg:gap-2">
+
+         <div class="bg-red-400  h-full lg:w-24 w-10"></div>
+         <div class="bg-red-400  h-full lg:w-24 w-10"></div>
+         <div class="bg-red-400  h-full lg:w-24 w-10"></div>
+         <div class="bg-red-400  h-full lg:w-24 w-10 "></div>
+         <div class="bg-red-400  h-full lg:w-24 w-10 "></div>
+
+    </div>
+    
+    <div class="bg-gray-500  lg:flex  1 rounded-3xl ">
+        <div class="flex flex-col items-center">
+          <div class="bg-red-400 lg:h-full h-56 rounded-3xl lg:w-72  w-72 "></div>
+        </div>
+        <div class=" bg-purple-600 rounded-3xl flex grow flex-col">
+           <div class="lg:h-14 w-full bg-red-500 h-6"></div>
+           <div class=" bg-purple-600 lg:grow h-36"></div>
+           <div class="lg:h-10 w-full bg-pink-500 h-8"></div>
+        </div>
+      </div>
+      <div class="bg-gray-500  lg:flex  1 rounded-3xl ">
+        <div class="flex flex-col items-center">
+          <div class="bg-red-400 lg:h-full h-56 rounded-3xl lg:w-72  w-72 "></div>
+        </div>
+        <div class=" bg-purple-600 rounded-3xl flex grow flex-col">
+           <div class="lg:h-14 w-full bg-red-500 h-6"></div>
+           <div class=" bg-purple-600 lg:grow h-36"></div>
+           <div class="lg:h-10 w-full bg-pink-500 h-8"></div>
+        </div>
+      </div>
+      <div class="bg-gray-500  lg:flex  1 rounded-3xl ">
+        <div class="flex flex-col items-center">
+          <div class="bg-red-400 lg:h-full h-56 rounded-3xl lg:w-72  w-72 "></div>
+        </div>
+        <div class=" bg-purple-600 rounded-3xl flex grow flex-col">
+           <div class="lg:h-14 w-full bg-red-500 h-6"></div>
+           <div class=" bg-purple-600 lg:grow h-36"></div>
+           <div class="lg:h-10 w-full bg-pink-500 h-8"></div>
+        </div>
+      </div>
+      <div class="bg-gray-700  flex flex-row lg:hidden">
+         <div class="bg-red-500  h-56 w-32"></div>
+          <div class="bg-purple-500  h-56 w-32"></div>
+         <div class="bg-black grow"></div>
+
+       </div>
+      
+    </>
+  )
+}
