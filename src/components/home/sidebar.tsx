443f380e0da47a1207eeb69490b127285b428056
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-08 23:21:58 +0530

    Sidebar

---
+++ src/components/home/sidebar.tsx
@@ -0,0 +1,23 @@
+export default function sidebar() {
+  return (
+    <>
+      <div class=" lg:flex hidden flex-col bg-purple-500 w-56 gap-3 pt-3">
+        <h1 class=" pt-10 pl-6 text-white font-bold">E@Crouse</h1>
+        <div class=" bg-purple-500 flex  flex-col grow gap-3 items-center pt-28">
+        <div class="bg-white h-12 w-52"></div>
+        <div class="bg-white h-12 w-52"></div>
+        <div class="bg-white h-12 w-52"></div>
+        <div class="bg-white h-12 w-52"></div>
+        <div class="bg-white h-12 w-52"></div>
+
+     </div>
+     <div class="  bg-purple-500 w-full flex gap-4 flex-col items-center pb-3">
+        <div class="bg-white h-12 w-52"></div>
+        <div class="bg-white h-12 w-52"></div>
+     </div>
+
+</div>
+
+    </>
+  )
+}
